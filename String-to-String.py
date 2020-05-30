89d28ba427c0468b2fdb882ae03964c2fb4cf5c9
MaciekMarcin <32327154+MaciekMarcin@users.noreply.github.com>
2020-05-30 22:25:43 +0200

    Add files via upload
    Added hill climbing

--- String-to-String.py
+++ String-to-String.py
@@ -3,6 +3,7 @@ import math
 from datetime import datetime
 import numpy as np
 from itertools import combinations
+import copy
 
 #Utowrzenie latin square
 def create_square(size: int):
@@ -24,7 +25,7 @@ def problem(i,points_v2_normal):
     print('Generated problem: ', *points_v2_normal,sep='\n',end='\n\n')
 
 def get_rid_of_strings(points_v2_wo_strings):
-    global wo_strings 
+    #global wo_strings 
     points_v2_wo_strings = [[int(i) if i.isdigit() else i for i in j] for j in points_v2_wo_strings]
     #print(points_v2_wo_strings)
     wo_strings = points_v2_wo_strings
@@ -41,24 +42,8 @@ def solution_v2(points_v2_normal):
         i = 0
         #print(points)
         if(check_for_x == 0):
-            #for row in outcome:
-            #    if(set(points).issubset(set(row))):
-            #        break
-            #    else:
-            #        i = i + 1
-            #print(i)
-            #print(size)
-            #if(i == size):
             outcome.append(points)
         elif(check_for_x == size):
-            #for row in outcome:
-            #    if(set(points).issubset(set(row))):
-            #        break
-            #    else:
-            #        i = i + 1
-            #print(i)
-            #print(size)
-            #if(i == size):
             whole_row_in_x_counter = whole_row_in_x_counter + 1
             outcome.append(points)
         else:
@@ -72,20 +57,14 @@ def solution_v2(points_v2_normal):
                     break
                 rotate_list = x
                 rotate_list = rotate_list[each:] + rotate_list[:each]
-                #print(rotate_list)
                 Common_elements = [i for i, j in zip(rotate_list, points) if i == j]
                 similarity = len(Common_elements)/size
-                #print(similarity)
                 if(similarity > Highest_similarity):
                     outcome.append(rotate_list)
-                #elif(points.count('x') < 1):
-                #    outcome.append(points)
-                #print(points)
-                #print('--------------------------------')
     print('OUTCOME')
     print(outcome)
     print(whole_row_in_x_counter)
-    #print(points_v2_normal)
+
     if(whole_row_in_x_counter == size):
         print('Incorrect input')
         quit()
@@ -100,53 +79,89 @@ def solution_v2(points_v2_normal):
     if(whole_row_in_x_counter == 0):
         #outcome_final = outcome
         print('Final outcome')
-        print(outcome)
+        for row in outcome:
+            print(row)
+        #print(outcome)
         result = outcome
         return result
 
+def climbing(points_v2_climbing):
+    #Stworzenie wszystkich możliwych rozwiązań z otoczenia
+    #Iterować po każdym punkcie gdzie jest x i forem sprawdzać która wartość zwraca najlepszy wynik i jechać dalej
+    outcome = []
+    for points in points_v2_climbing:
+        for point in points:
+            print(point)
+            if(point == 'x'):
+                final_point_score = solution_t(points_v2_climbing,problem_amount)
+                print(final_point_score)
+                for i in range(1,3+1):
+                    print('i: ' + str(i))
+                    loop_list = copy.deepcopy(points_v2_climbing) #jedyna opcja działająca kopiowania listy
+                    #Trzeba pracować na kopii listy niż na oryginale
+                    loop_list[loop_list.index(points)][points.index(point)] = i
+                    score = solution_t(loop_list,problem_amount)
+                    print('score of point: ' + str(score))
+                    if(score > final_point_score):
+                        final_point_score = score
+                        points_v2_climbing[points_v2_climbing.index(points)][points.index(point)] = int(i)
+                        print('inserted ' + str(i) + ' at ['+ str(points_v2_climbing.index(points)) + ',' + str(points.index(i)) + ']')
+                        print('example ' + str(points_v2_climbing))
+                        break
+    print(points_v2_climbing) #print final list
+    return points_v2_climbing
 
-    #Dla normal
-    #print('Normal: ', *solution, sep='\n', end='\n\n')
-    #print(*points_v2_normal_reversed, sep='\n', end='\n\n')
-    #points_v2_transposed = list(map(list, zip(points_v2_normal[0],points_v2_normal[1],points_v2_normal[2])))
-def format_outcome(outcome):
-    solution = solution_v2(outcome)
-    print(*outcome, sep='\n', end='\n\n')
+
+def tabu(points_v2_normal):
+    print('test')
+
+#def format_outcome(outcome):
+#    solution = solution_v2(outcome)
+#    print(*outcome, sep='\n', end='\n\n')
 
 
-def solution_t(points_v2_normal,problem_amount):
+def solution_t(points_v2_score,problem_amount):
     i = 0
     j = 0
-    for points in points_v2_normal:
+    for points in points_v2_score:
         for point in points:
             elements = len(points)
-            if(points.count(point) == 1):
+            if(point == 'x'):
+                i = i# - 1
+            elif(points.count(point) == 1):
                 i = i + 1
             else:
-                i = i #+ (1/2)
-    points_v2_transposed = np.transpose(points_v2_normal)
+                i = i - 1
+    points_v2_transposed = np.transpose(points_v2_score)
     points_v2_transposed = points_v2_transposed.tolist()
     for points in points_v2_transposed:
         for point in points:
             elements = len(points)
-            if(points.count(point) == 1):
+            if(point == 'x'):
+                i = i# - 1
+            elif(points.count(point) == 1):
                 j = j + 1
             else:
-                j = j #+ (1/2)
-    print(i)
-    print(j)
-    score = i + j
-    score = round((i + j)/(amount_of_elements*2),2)*100
-    print(score,'%',sep='')
+                j = j - 1#+ (1/2)
+    print('I_SCORE: ' + str(i))
+    print('J_SCORE: ' + str(j))
+    score = (i + j)/18
+    #score = round((i + j)/(amount_of_elements*2),2)*100
+    #print(score,'%',sep='')
     return score
 
 def load_from_file():
-    text_file_source = open('Source.txt', 'r').read().replace('\n','')
+    text_file_source = open('Source.txt', 'r').read()#.replace('\n','')
+    size = len(text_file_source)
     #data = open('Source.txt', 'r').read().replace('\n','')
-    for line in text_file_source:
-        stripped_line = line.strip()
-        line_list = stripped_line.split()
-        points_from_txt.append(line_list)
+    for line in range(0,3):
+        temp_list = []
+        for item in text_file_source:
+            print(item)
+            #stripped_line = line.strip()
+            #line_list = stripped_line.split()
+            temp_list.append(item)
+        points_from_txt.append(temp_list)
     print(points_from_txt)
 
 
@@ -158,6 +173,7 @@ new_points_transposed = []
 problem_amount = 0
 solution_amount = 0
 trial_amount = int(input('How many test cases?: '))
+algorithm = int(input('Select algorithm:\n1-Bruteforce\n2-Climbing\n'))
 source = int(input('Select data source:\n1-From txt file\n2-Default source data\n'))
 size = int(input('Input number (not applicable if you want input from file): '))
 problem_amount = int(input('How many unknown values do you want? (Max amount is: ' + str(size*size) + '): '))
@@ -184,13 +200,17 @@ def trial(trial_amount, problem_amount):
         points_v2_normal = points_v2
         
         problem(problem_amount,points_v2_normal)
+        points_v2_climbing = copy.deepcopy(points_v2_normal) # zbędna linia bo tylko kopiuje referencje i puszczam jeden algorytm naraz
         text_file.write('Problem ' + str(each + 1) + ':\n')
         for row in points_v2_normal:
             text_file.write(str(row) + '\n')
         text_file.write('\n')
-        final_outcome = solution_v2(points_v2_normal)
-        print(final_outcome)
-        score = solution_t(points_v2_normal,problem_amount)
+        #######FROM HERE ONWARD BRUTEFORCE############
+        if(algorithm == 1):
+            final_outcome = solution_v2(points_v2_normal)
+        elif(algorithm == 2):
+            final_outcome = climbing(points_v2_climbing)
+        score = solution_t(final_outcome,problem_amount)
         after = datetime.now()
         interval = after - before
         if(type(interval_sum) == int):
@@ -202,7 +222,7 @@ def trial(trial_amount, problem_amount):
         else:
             score_sum = score_sum + score
         text_file.write('Latin square ' + str(each + 1) + ':\n')
-        for row in points_v2_normal:
+        for row in final_outcome:
             text_file.write(str(row) + '\n')
         text_file.write('Time: ' + str(interval) + '\nScore: ' + str(round(score,0)) + '%\n')
         text_file.write('\n--------------------------------------------------------\n\n')
@@ -212,7 +232,7 @@ def trial(trial_amount, problem_amount):
         i = i + 1
     Avg_score = round((score_sum/trial_amount),2)
     Avg_time = (interval_sum/trial_amount)
-    text_file.write('Total time: ' + str(interval_sum) + '%\nAverage time: ' + str(Avg_time) + '\nAverage score: ' + str(Avg_score) + '%\n\n')
+    text_file.write('Total time: ' + str(interval_sum) + '\nAverage time: ' + str(Avg_time) + '\nAverage score: ' + str(Avg_score) + '%\n\n')
     text_file.close()
     print(interval_sum)
 
